5bc1d7cd3942adfa51d6527900c0950a4e0dd5a9
AkmalGilangRosadi <83489612+AkmalGilangRosadi@users.noreply.github.com>
2022-09-20 22:25:20 +0700

    prak4

--- Akmal Gilang Rosadi_2067051001_Prakweblanjut/app/Models/MahasiswaModel.php
+++ Akmal Gilang Rosadi_2067051001_Prakweblanjut/app/Models/MahasiswaModel.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class MahasiswaModel extends Model{
+class MahasiswaModel extends Model
+{
     protected $table = 'mahasiswa';
     protected $primaryKey = 'id_mahasiswa;';
     protected $useTimeStamp = true;
+
+
+    protected $useAutoIncrement = true;
+    protected $allowedFields    = ['npm', 'nama', 'alamat', 'created_at', 'update_at'];
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'update_at';
 }
